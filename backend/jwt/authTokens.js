f86d8721c657e4750199b21c5e987a09fde29a0e
Shrivastavjii <157983312+Shrivastavjii@users.noreply.github.com>
2025-09-18 16:51:49 +0530

    Update authTokens.js

--- backend/jwt/authTokens.js
+++ backend/jwt/authTokens.js
@@ -8,9 +8,9 @@ export const createTokenAndSaveCookies= async(userId,res)=>{
     })
    
   res.cookie('jwt', token, {
-  httpOnly: true,
+  httpOnly: false,
   secure: false,
-  sameSite: "strict",
+  sameSite: "none",
   //maxAge: 7 * 24 * 60 * 60 * 1000,
 });
 
